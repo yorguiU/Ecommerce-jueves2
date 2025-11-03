475d10457c7171abb40d14fdbca119f03a4b2dfe
yorgui sanchez <yorguisanchez3@gmail.com>
2025-11-03 13:18:32 -0500

    Rutas Basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return "LISTADO DE PRODUCTOS";
+});
+
+Route::get('/products/create', function () {
+    return "FORMULARIO DE CREACIÓN DE PRODUCTOS";
+});
+
+Route::get('products/{id}/{category?}', function($id, $category = null){
+    if ($category == null){
+        return "Detalle de cada producto: " . $id;
+    }else{
+        return "Detalle de cada producto: " . $id . " de la categoria: " . $category;
+    }
+});
+
