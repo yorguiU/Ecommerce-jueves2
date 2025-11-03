59a1d22a6f22c63b836e2e4823187cf7aeb838e7
yorgui sanchez <yorguisanchez3@gmail.com>
2025-11-03 14:40:33 -0500

    vistas basicas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,19 +7,15 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     function index(){
-        return "LISTADO DE PRODUCTOS";
+        return view('products.index');
     }
 
     function create(){
-        return "FORMULARIO DE CREACION DE PRODUCTOS";
+        return view('products.create');
     }
 
     function show($id, $category = null){
-    if ($category == null){
-        return "Detalle de cada producto: " . $id;
-    }else{
-        return "Detalle de cada producto: " . $id . " de la categoria: " . $category;
-    }
+    return view('products.show',);
 }
 }
 
