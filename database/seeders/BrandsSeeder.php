0c10b337a67fe6df82696556f3c72a801e8b25c5
yorgui sanchez <yorguisanchez3@gmail.com>
2025-11-14 15:21:12 -0500

    ajuste de los seeder

---
+++ database/seeders/BrandsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Brand;
+
+class BrandsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Brand::factory(50)->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class
+            CategorySeeder::class,
+            BrandsSeeder::class,
         ]);
 
 
